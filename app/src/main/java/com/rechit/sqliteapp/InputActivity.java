64b007f4091d81f3b10be821bc08371b6a743f2d
Farhan Mufti Hilmy <farhanmuftihilmy@gmail.com>
2020-11-27 10:29:09 +0700

    Initial commit

---
+++ app/src/main/java/com/rechit/sqliteapp/InputActivity.java
@@ -0,0 +1,40 @@
+package com.rechit.sqliteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class InputActivity extends AppCompatActivity {
+    EditText editNama;
+    EditText editAlamat;
+    Button buttonSubmit;
+    DatabaseHelper databaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+        editNama = findViewById(R.id.edit_nama);
+        editAlamat = findViewById(R.id.edit_alamat);
+        buttonSubmit = findViewById(R.id.btn_submit);
+        databaseHelper = new DatabaseHelper(this);
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitData();
+            }
+        });
+    }
+
+    private void submitData(){
+        //panggil method databasehelper
+        if(editNama.getText().toString().length() > 0 &&
+        editAlamat.getText().toString().length() > 0){
+            databaseHelper.addStudent(editNama.getText().toString(), editAlamat.getText().toString());
+            onBackPressed();
+        }
+    }
+}
